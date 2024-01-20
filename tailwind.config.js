4f257e2a736f13ce2b6b82d6b984e9f3cd509ef0
ddaeyeonkim <improve777@gmail.com>
2024-01-20 22:35:55 +0900

    tailwind css 구성 추가

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./index.html', './web/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: true,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
--- web/views/HomeView.vue
+++ web/views/HomeView.vue
@@ -5,5 +5,16 @@ import TheWelcome from '../components/TheWelcome.vue'
 <template>
   <main>
     <TheWelcome />
+    <div class="flex flex-row">
+      <div class="basis-1/4 btn">01</div>
+      <div class="basis-1/4 btn">02</div>
+      <div class="basis-1/2 btn">03</div>
+    </div>
   </main>
 </template>
+
+<style scoped>
+.btn {
+  @apply px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-red-700;
+}
+</style>
